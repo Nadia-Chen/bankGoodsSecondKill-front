22bcae0eb0539e289837cb878fbf27b1ef6a7a8d
Nadia-Chen <nadia2000lavigner@outlook.com>
2022-04-15 15:09:10 +0800

    April 15

--- src/components/menus/MyGoods.vue
+++ src/components/menus/MyGoods.vue
@@ -8,16 +8,40 @@
       <button type="button" class="btn btn-light" style="marginRight:10px;background-color: #FFE4E1" @click="onShowaccept">确认收货</button>
       <button type="button" class="btn btn-light" style="marginRight:10px;background-color: #E6E6FA" @click="onShowback">售后</button>
   </div>
+  <table class="table table-bordered table-striped table-hover">
+    <thead>
+      <!-- 商品秒杀列表展示数据：商品名称、商品价格、商品介绍、剩余数量、秒杀倒计时、秒杀 -->
+      <tr>
+        <th>#</th>
+        <th>商品描述</th>
+        <th>商品购买数量</th>
+        <th>商品价格</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      <tr v-for="(item, i) in goodslist" :key="item.id">
+        <td>{{ i + 1 }}</td>
+        <td>{{ item.description }}</td>
+        <td>{{ item.num }}</td>
+        <td>{{ item.price }}</td>
+       
+      </tr>
+    </tbody>
+  </table>
+
 </template>
 
 <script>
 export default {
   name: 'MyGoods',
-  // data() {
-  //   return {
-
-  //   }
-  // },
+  data() {
+    return {
+      goodslist: [
+        { id: 1, description: 'string', num: '1', price: '111'},
+      ],
+    }
+  },
 
   methods: {
     onShowall(){
